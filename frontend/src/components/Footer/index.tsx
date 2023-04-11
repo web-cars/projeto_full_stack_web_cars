b3e97d5afc6f93c3febd3b955d2eb04ff664f97d
daniel delos silva <apeiron52@gmail.com>
2023-04-11 17:05:36 -0300

    Refactor:footerPage responsive

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,10 +1,11 @@
-import { Box, Flex, Text, Icon } from "@chakra-ui/react";
+import { Box, Flex, Text, Icon, useMediaQuery } from "@chakra-ui/react";
 import { ChevronUpIcon } from '@chakra-ui/icons'
 
 const Footer = () => {
+  const [isLargerThan768] = useMediaQuery("(min-width: 768px)");
   return (
     <Box bg={"black"} w="100%" position="fixed" bottom="0">
-      <Flex h="10vh" alignItems="center" justifyContent="space-between" px="46.5px" color="white">
+      <Flex h="10vh" alignItems="center" justifyContent="space-between" px="46.5px" color="white" flexDirection={isLargerThan768 ? "row" : "column"}>
         <Box>
           <Text as="span" fontSize={"x-large"} fontWeight={"semibold"} mr={2}>Motors</Text>
           <Text as="span">Shop</Text>
