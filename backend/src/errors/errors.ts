2fdcf60101533e5c2f28e61d41fbc1cdc0c2c0fb
Garcia <garciasholding@gmail.com>
2023-04-11 16:18:37 -0300

    fix: message format to object instead of string

--- backend/src/errors/errors.ts
+++ backend/src/errors/errors.ts
@@ -1,14 +1,9 @@
-import { NextFunction, Request, Response } from "express"
-
-
 export class AppError extends Error {
+  statusCode: number;
 
-    statusCode: number
-    
-    constructor(message: string, statusCode: number = 400){
-        super()
-        this.message = message
-        this.statusCode = statusCode
-    }
+  constructor(message: string, statusCode: number = 400) {
+    super();
+    this.message = message;
+    this.statusCode = statusCode;
+  }
 }
-
