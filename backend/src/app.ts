b8a7919a136032a66b900383813d3b01d5bf3e0d
Garcia <garciasholding@gmail.com>
2023-04-11 16:14:49 -0300

    feat: car advertisement update /carads/:id

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,13 @@
 import "express-async-errors";
 import "reflect-metadata";
-import express, { Request, Response } from "express";
-
+import express from "express";
 import cors from "cors";
+import { errorHandler } from "./errors/errorHandler";
+import { carAdsRoutes } from "./routes/carAds.routes";
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use("/carads", carAdsRoutes);
+app.use(errorHandler);
 
 export default app;
