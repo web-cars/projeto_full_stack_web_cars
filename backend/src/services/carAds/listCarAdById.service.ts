f86d1e6ab9b5836e8a4e8ab5425779835f03e255
daniel delos silva <apeiron52@gmail.com>
2023-04-12 12:22:14 -0300

    Refactor: listAds type idCardAd space

--- backend/src/services/carAds/listCarAdById.service.ts
+++ backend/src/services/carAds/listCarAdById.service.ts
@@ -1,11 +1,11 @@
 import { Repository } from "typeorm";
-import  AppDataSource  from "../../data-source";
+import AppDataSource from "../../data-source";
 import { CarAds } from "../../entities/carAds.entity";
 
-const listCarAdByIdService = async(idCarAd: string): Promise<any> => {
-    const carAdRepo: Repository<CarAds> = AppDataSource.getRepository(CarAds)
-    const carAd = await carAdRepo.findOneBy({id:idCarAd})
-    return carAd
-}
+const listCarAdByIdService = async (idCarAd: string): Promise<any> => {
+  const carAdRepo: Repository<CarAds> = AppDataSource.getRepository(CarAds);
+  const carAd = await carAdRepo.findOneBy({ id: idCarAd });
+  return carAd;
+};
 
-export default listCarAdByIdService
+export default listCarAdByIdService;
