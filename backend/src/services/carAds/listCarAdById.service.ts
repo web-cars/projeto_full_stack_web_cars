5b67c98be74a56d39d7ca814247ee7dfed36da9a
daniel delos silva <apeiron52@gmail.com>
2023-04-12 12:40:07 -0300

    Refactor: listAds type Promisse

--- backend/src/services/carAds/listCarAdById.service.ts
+++ backend/src/services/carAds/listCarAdById.service.ts
@@ -1,8 +1,9 @@
 import { Repository } from "typeorm";
 import AppDataSource from "../../data-source";
 import { CarAds } from "../../entities/carAds.entity";
+import { ICarAdResponse } from "../../interfaces/carAds.interfaces";
 
-const listCarAdByIdService = async (idCarAd: string): Promise<any> => {
+const listCarAdByIdService = async (idCarAd: string): Promise<ICarAdResponse> => {
   const carAdRepo: Repository<CarAds> = AppDataSource.getRepository(CarAds);
   const carAd = await carAdRepo.findOneBy({ id: idCarAd });
   return carAd;
