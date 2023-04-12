49c01de2da3bd49bcd31eebb5bfd8d2fdd2d7824
Thaisa Alice <thaisalic3@icloud.com>
2023-04-12 17:04:43 -0300

    styled: added icons for dark mode button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,13 @@
 
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, DarkMode } from '@chakra-ui/react'
 import { theme } from './style/theme'
-import { InfoAd } from './components/InfoAd/index';
+import { ButtonMode } from './components/DarkMode';
 function App() {
 
 
   return (
     <ChakraProvider theme={theme}>
-      <InfoAd></InfoAd>
+      <ButtonMode></ButtonMode>
     </ChakraProvider>
   )
 }
--- frontend/src/components/DarkMode/index.tsx
+++ frontend/src/components/DarkMode/index.tsx
@@ -1,17 +1,29 @@
-import { Flex, HStack, Button, useColorMode, LightMode } from '@chakra-ui/react'
+import { SunIcon, MoonIcon } from '@chakra-ui/icons'
+import { 
+  Flex,
+  HStack, 
+  useColorMode, 
+  Button 
+} from '@chakra-ui/react'
 
 export const ButtonMode = () => {
 
-    const { toggleColorMode } = useColorMode()
+    const { colorMode, toggleColorMode } = useColorMode()
 
     return (
         <Flex height={'10vh'} justifyContent={'flex-end'} alignItems={'center'} >
             <HStack gap={'5'} marginEnd={'10px'}>
-                <LightMode>
-                    <Button colorScheme={'purple'} onClick={toggleColorMode}>
-                        Change Theme
-                    </Button>
-                </LightMode>
+              <Button 
+                onClick={() => toggleColorMode()}
+                pos={'absolute'}
+                top={'0'}
+                right={'0'}
+                m={'1rem'}
+              >
+                {colorMode === 'dark' 
+                  ? <SunIcon/> 
+                  : <MoonIcon/>}
+              </Button>
             </HStack>
         </Flex>
         )
