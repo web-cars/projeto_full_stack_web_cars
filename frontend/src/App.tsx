a5dca850a3276b34809d8045254efe2a0389c129
Thaisa Alice <thaisalic3@icloud.com>
2023-04-12 16:39:17 -0300

    styled: ad information card

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from './style/theme'
+import { InfoAd } from './components/InfoAd/index';
 function App() {
 
 
   return (
     <ChakraProvider theme={theme}>
+      <InfoAd></InfoAd>
     </ChakraProvider>
   )
 }
