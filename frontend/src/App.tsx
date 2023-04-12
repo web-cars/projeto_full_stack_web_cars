ab8c7f2be421a026d545c2e89ac0a292faca4531
Thaisa Alice <thaisalic3@icloud.com>
2023-04-12 13:11:01 -0300

    feat: new branch card

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,11 @@
 
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from './style/theme'
-import { StyleModal } from './components/Modal/index';
-import { Image } from '@chakra-ui/react';
-import Car from './assets/carexample.svg'
 function App() {
 
 
   return (
     <ChakraProvider theme={theme}>
-      <StyleModal>
-        <Image src={Car} alt='Carro exemplo' />
-      </StyleModal>
     </ChakraProvider>
   )
 }
