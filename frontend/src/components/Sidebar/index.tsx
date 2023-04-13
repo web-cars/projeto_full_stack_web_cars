ba68163a67e022da98d46f2f74a3baffe993c5a4
daniel delos silva <apeiron52@gmail.com>
2023-04-13 17:07:27 -0300

    Fix: position fix to none

--- frontend/src/components/Sidebar/index.tsx
+++ frontend/src/components/Sidebar/index.tsx
@@ -7,37 +7,37 @@ const Sidebar = () => {
     const colors = ["Azul", "Branca", "Cinza", "Prata", "Preta", "Verde"]
     const years = ["2022", "2021", "2018", "2015", "2013"]
      return (
-    <Box bg="white" color="black" w="400px" h="100vh" pos="fixed" top="0" left="0" p="4" overflowY="auto">
+    <Box bg="white" color="black" w="400px" h="100vh" left="0" p="4">
         <Flex justifyContent="space-around" align="center" display={isLargerThan768 ? "none" : "block"}>
             <Text >Filtros</Text>
             <Text  color="gray.300">x</Text>
         </Flex>
-      <List mt="50px" ml="3">
+      <List mt="5px" ml="3" fontSize="small">
         <ListItem mb="4" fontWeight="bold">Marca
-            <List ml="4" mt="4" color="gray.300">{brands.map((marca, index) => (<ListItem key={index}>{marca}</ListItem>))}</List>
+            <List ml="4" mt="4" color="gray.500">{brands.map((marca, index) => (<ListItem key={index}>{marca}</ListItem>))}</List>
         </ListItem>
         <ListItem mb="4" fontWeight="bold">Modelo
-            <List ml="4" mt="4" color="gray.300">{models.map((marca, index) => (<ListItem key={index}>{marca}</ListItem>))}</List>
+            <List ml="4" mt="4" color="gray.500">{models.map((marca, index) => (<ListItem key={index}>{marca}</ListItem>))}</List>
         </ListItem>
         <ListItem mb="4" fontWeight="bold">Cor
-            <List ml="4" mt="4" color="gray.300">{colors.map((marca, index) => (<ListItem key={index}>{marca}</ListItem>))}</List>
+            <List ml="4" mt="4" color="gray.500">{colors.map((marca, index) => (<ListItem key={index}>{marca}</ListItem>))}</List>
         </ListItem>
         <ListItem mb="4" fontWeight="bold">Ano
-            <List ml="4" mt="4" color="gray.300">{years.map((marca, index) => (<ListItem key={index}>{marca}</ListItem>))}</List>
+            <List ml="4" mt="4" color="gray.500">{years.map((marca, index) => (<ListItem key={index}>{marca}</ListItem>))}</List>
         </ListItem>
         <ListItem mb="4" fontWeight="bold">Km
             <List mt="4">
                 <ListItem maxW="400px">
-                    <Input bg="gray.300" w="38%"  placeholder='Mínima' border="none" mr="2" borderRadius="0"/>
-                    <Input bg="gray.300" w="38%"  placeholder='Máxima' border="none" ml="" borderRadius="0"/>
+                    <Input bg="gray.300" w="38%"  placeholder='Mínima' border="none" mr="2" borderRadius="0"  fontSize="small"/>
+                    <Input bg="gray.300" w="38%"  placeholder='Máxima' border="none" ml="" borderRadius="0"  fontSize="small"/>
                 </ListItem>
             </List>
         </ListItem>
         <ListItem mb="4" fontWeight="bold">Preço
             <List mt="4">
                 <ListItem maxW="400px"> 
-                    <Input bg="gray.300" w="38%"  placeholder='Mínimo' border="none" mr="2" borderRadius="0" />
-                    <Input bg="gray.300" w="38%"  placeholder='Máximo' border="none" ml="" borderRadius="0"/>
+                    <Input bg="gray.300" w="38%"  placeholder='Mínimo' border="none" mr="2" borderRadius="0"  fontSize="small"/>
+                    <Input bg="gray.300" w="38%"  placeholder='Máximo' border="none" ml="" borderRadius="0"  fontSize="small"/>
                 </ListItem>
             </List>
         </ListItem>
