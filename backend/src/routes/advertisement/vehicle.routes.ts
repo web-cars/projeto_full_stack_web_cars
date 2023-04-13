0545be377c1ad269e332d9508d0e62905a3ccf8b
Guilherme Schulz <gui.schulz@gmail.com>
2023-04-13 10:17:30 -0300

    fix: fixed route to all controllers

--- backend/src/routes/advertisement/vehicle.routes.ts
+++ backend/src/routes/advertisement/vehicle.routes.ts
@@ -1,13 +1,14 @@
-import advertisementsCreateController, {
-  deleteCarAdController,
-  getSpecificCarController,
-} from "../../controllers/advertisement/advertisement.controller";
 import { Router } from "express";
 import { verifyIfCarAdExistsMiddleware } from "../../middlewares/verifyIfCarAdExists.middleware";
 import { validSerializerMiddleware } from "../../middlewares/validSerializer.middleware";
 import { carAdUpdateSerializer } from "../../serializers/carAds.serializers";
-import { updateCarAdController } from "../../controllers/advertisement/advertisement.controller";
-import { listAllCarAdsController } from './../../controllers/advertisement/advertisement.controller';
+import {
+  advertisementsCreateController,
+  deleteCarAdController,
+  getSpecificCarController,
+  updateCarAdController,
+} from "../../controllers/advertisement/advertisement.controller";
+import { listAllCarAdsController } from "./../../controllers/advertisement/advertisement.controller";
 
 const advertisementRoutes = Router();
 
@@ -29,6 +30,5 @@ advertisementRoutes.delete(
   verifyIfCarAdExistsMiddleware,
   deleteCarAdController
 );
-
 
 export default advertisementRoutes;
