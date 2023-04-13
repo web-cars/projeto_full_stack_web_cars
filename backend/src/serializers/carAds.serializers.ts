14021dbbdedddb9f81ca11ce6decf4f4b602fdd4
Guilherme Schulz <gui.schulz@gmail.com>
2023-04-13 10:45:27 -0300

    feat: added serializer to create car ads

--- backend/src/serializers/carAds.serializers.ts
+++ backend/src/serializers/carAds.serializers.ts
@@ -22,3 +22,32 @@ export const carAdUpdateSerializer = z.object({
   fuel_type: z.nativeEnum(FuelType).optional(),
   images: imagesUpdateSerializer.array().optional(),
 });
+
+export const imagesCreateSerializer = z.object({
+  file: z.string(),
+});
+
+export const carAdCreateSerializer = z.object({
+  price: z
+    .string()
+    .regex(
+      priceRegex,
+      "Price must be a number with a maximum of 2 decimal places"
+    )
+    .transform((value) => parseFloat(value)),
+  isActive: z.boolean().default(true),
+  description: z.string().max(256).optional(),
+  color: z.string().max(25),
+  model: z.string().max(256),
+  brand: z.nativeEnum(Brand),
+  year: z.nativeEnum(Year),
+  fuel_type: z.nativeEnum(FuelType),
+  images: z.array(imagesCreateSerializer).nonempty(),
+  fipePrice: z
+    .string()
+    .regex(
+      priceRegex,
+      "Price must be a number with a maximum of 2 decimal places"
+    )
+    .transform((value) => parseFloat(value)),
+});
