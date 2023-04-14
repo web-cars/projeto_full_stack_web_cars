262dddf1c16944ee19d647f29e5302851608a205
rinaldosilvino <rinaldosilvino2@gmail.com>
2023-04-14 08:51:31 -0300

    add navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,15 +2,21 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from './style/theme'
 import { ButtonMode } from './components/DarkMode/index';
 import Header from './components/Header';
+import Navbar from './components/Navbar';
 
 function App() {
 
 
   return (
-    <ChakraProvider theme={theme}>
-      <Header/>
-      <ButtonMode/>
-    </ChakraProvider>
+    <main>
+      <ChakraProvider theme={theme}>
+          <Navbar/>
+          <Header/>
+          <ButtonMode/>
+      </ChakraProvider>
+
+    </main>
+
   )
 }
 
---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,63 @@
+import { useNavigate } from "react-router-dom";
+import logo from "../../assets/img/motors-shop.svg"
+import { HiMenu } from "react-icons/hi";
+import { IoClose } from "react-icons/io5";
+
+import {
+  NavStyled,
+  MenuIconStyled,
+  MenuLinks,
+  Logotipo,
+  NavContent,
+  ButtonLoginStyled,
+  ButtonRegisterStyled,
+  DivLineHeaderStyled,
+} from "./style";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  const navigate = useNavigate();
+  const [visible, setVisible] = useState(false);
+  return (
+    <NavStyled>
+      <NavContent>
+        <Logotipo className="logo">
+          <Link to="/home">
+              <img src={logo} alt="" />
+          </Link>
+        </Logotipo>
+
+        <MenuIconStyled onClick={() => setVisible(!visible)}>
+          <HiMenu size={28} />
+        </MenuIconStyled>
+
+        <MenuLinks visible={visible}>
+          <IoClose size={28} onClick={() => setVisible(false)} />
+          <li>
+            <a href={"#cars"}>Carros</a>
+          </li>
+          <li>
+            <a href={"#motorcycle"}>Motos</a>
+          </li>
+          <li>
+            <a href={"auction"}>Leilão</a>
+          </li>
+
+          <DivLineHeaderStyled></DivLineHeaderStyled>
+
+          <Link to="/login">
+            <ButtonLoginStyled>Fazer Login</ButtonLoginStyled>
+          </Link>
+
+          <Link to="/register">
+            {" "}
+            <ButtonRegisterStyled>Cadastrar</ButtonRegisterStyled>
+          </Link>
+        </MenuLinks>
+      </NavContent>
+    </NavStyled>
+  );
+};
+
+export default Navbar;
