2175a4622fff1698b398271db076b390157f2570
Thaisa Alice <thaisalic3@icloud.com>
2023-04-19 13:37:42 -0300

    chore: uploading new branch

--- frontend/src/components/CardProfile/index.tsx
+++ frontend/src/components/CardProfile/index.tsx
@@ -1,7 +1,6 @@
 
 import { ICardProfile } from './../../interface/index';
 import Mock from '../../mock/mock.json'
-
 import {
   Card,
   CardBody,
@@ -11,7 +10,6 @@ import {
   Text,
   CardFooter,
   Button,
-
   Box
 } from '@chakra-ui/react'
 import { css } from '@emotion/react';
@@ -19,7 +17,7 @@ import { css } from '@emotion/react';
 export const CardProfile = ({ image, name, description }: ICardProfile) => {
   const divMobile = css`
   @media (max-width:1024px) {
-     width: 100%;
+    width: 100%;
     padding: 15px
   }
   `
