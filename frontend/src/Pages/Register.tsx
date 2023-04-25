353c7d6f0ee8fa5311a6a2433e46c95099aecfbe
daniel delos silva <apeiron52@gmail.com>
2023-04-25 00:16:59 -0300

    refactor: css page register

--- frontend/src/Pages/Register.tsx
+++ frontend/src/Pages/Register.tsx
@@ -4,13 +4,6 @@ import {
   Box,
   FormLabel,
   Input,
-  Modal,
-  ModalBody,
-  ModalCloseButton,
-  ModalContent,
-  ModalHeader,
-  ModalOverlay,
-  useDisclosure,
   Text,
   Textarea,
   Flex,
@@ -50,10 +43,7 @@ const schema = z.object({
 })
 
 const Register = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const initialRef = React.useRef(null);
-  const finalRef = React.useRef(null);
-  const [btnActive, setBtnActive] = useState("comprador");
+   const [btnActive, setBtnActive] = useState("comprador");
   const handleButtonClick = (button: React.SetStateAction<string>) => {
     setBtnActive(button);
   };
@@ -71,103 +61,106 @@ const Register = () => {
   return (
     <>
     <Navbar/>
-       <Flex justifyContent="center" alignItems="center" mb="5">
-         <FormControl w="25%">
-                <Box mt={4}>
-                  <FormLabel fontFamily={"Inter"} fontWeight="medium">Nome</FormLabel>
-                  <Input required {...register("name")} placeholder="Ex: Samuel Leão" />
-                  {errors.name && (<FormErrorMessage variant="default" >{errors.name.message}</FormErrorMessage>)}
-                  <FormHelperText>{errors.name?.message}</FormHelperText>
-                </Box>
-                <Box mt={4}>
-                  <FormLabel fontFamily={"Inter"} fontWeight="medium">Email</FormLabel>
-                  <Input {...register("email")} placeholder="Ex: samuel@kenzie.com.br"/>
-                  {errors.email && (<FormErrorMessage variant="default" >{errors.email.message}</FormErrorMessage>)}
-                  <FormHelperText>{errors.email?.message}</FormHelperText>
-                </Box>
-                <Box mt={4}>
-                  <FormLabel fontFamily={"Inter"} fontWeight="medium">CPF</FormLabel>
-                  <Input {...register("cpf")} placeholder="000.000.000-00" />
-                  {errors.cpf && (<FormErrorMessage variant="default" >{errors.cpf.message}</FormErrorMessage>)}
-                  <FormHelperText>{errors.cpf?.message}</FormHelperText>
-                </Box>
-                <Box mt={4}>
-                  <FormLabel fontFamily={"Inter"} fontWeight="medium">Celular</FormLabel>
-                  <Input {...register("cellphone")} placeholder="(DDD) 90000-0000"/>
-                </Box>
-                <Box mt={4}>
-                  <FormLabel fontFamily={"Inter"} fontWeight="medium">Data de nascimento</FormLabel>
-                  <Input {...register("birthDate")} placeholder="00/00/00" />
-                  {errors.birthDate && (<FormErrorMessage variant="default" >{errors.birthDate.message}</FormErrorMessage>)}
-                  <FormHelperText>{errors.birthDate?.message}</FormHelperText>
-                </Box>
-                <Box mt={4}>
-                  <FormLabel fontFamily={"Inter"} fontWeight="medium">Descrição</FormLabel>
-                  <Textarea {...register("description")} placeholder="Digitar descrição"/>
-                  {errors.description && (<FormErrorMessage variant="default" >{errors.description.message}</FormErrorMessage>)}
-                  <FormHelperText>{errors.description?.message}</FormHelperText>
-                </Box>
-                <Text fontFamily={"Inter"} fontWeight="medium" mt="5">Infomações de endereço</Text>
-                <Box mt={4}>
-                  <FormLabel fontFamily={"Inter"} fontWeight="medium">CEP</FormLabel>
-                  <Input {...register("address.cep")} placeholder="00000.000" />
-                </Box>
-                <Flex justifyContent="space-between">
-                  <Box mt={4} mr={4}>
-                    <FormLabel fontFamily={"Inter"} fontWeight="medium">Estado</FormLabel>
-                    <Input {...register("address.state")} placeholder="Digitar Estado"/>
+       <Box as="main" w="100%" bg={theme.colors.greyScale.grey8}>
+         <Flex justifyContent="center" alignItems="center">
+           <FormControl w="25%" mb="5" mt={4} bg={theme.colors.greyScale.whiteFixed} p="10">
+           <Text fontFamily={"Inter"} fontWeight="medium" mt={4}>Cadastro</Text>
+                  <Box mt={4}>
+                    <FormLabel fontFamily={"Inter"} fontWeight="medium">Nome</FormLabel>
+                    <Input required {...register("name")} placeholder="Ex: Samuel Leão" />
+                    {errors.name && (<FormErrorMessage variant="default" >{errors.name.message}</FormErrorMessage>)}
+                    <FormHelperText>{errors.name?.message}</FormHelperText>
                   </Box>
                   <Box mt={4}>
-                    <FormLabel fontFamily={"Inter"} fontWeight="medium">Cidade</FormLabel>
-                    <Input {...register("address.city")} placeholder="Digitar cidade"/>
-                  </Box>
-                </Flex>
-                <Box mt={4}>
-                  <FormLabel fontFamily={"Inter"} fontWeight="medium">Rua</FormLabel>
-                  <Input {...register("address.street")} placeholder="Digitar Rua"/>
-                </Box>
-                <Flex justifyContent="space-between">
-                  <Box mt={4} mr={4}>
-                    <FormLabel fontFamily={"Inter"} fontWeight="medium">Número</FormLabel>
-                    <Input {...register("address.number")} placeholder="Digitar número"/>
+                    <FormLabel fontFamily={"Inter"} fontWeight="medium">Email</FormLabel>
+                    <Input {...register("email")} placeholder="Ex: samuel@kenzie.com.br"/>
+                    {errors.email && (<FormErrorMessage variant="default" >{errors.email.message}</FormErrorMessage>)}
+                    <FormHelperText>{errors.email?.message}</FormHelperText>
                   </Box>
                   <Box mt={4}>
-                    <FormLabel fontFamily={"Inter"} fontWeight="medium">Complemento</FormLabel>
-                    <Input {...register("address.complement")} placeholder="Ex: apart 307"/>
+                    <FormLabel fontFamily={"Inter"} fontWeight="medium">CPF</FormLabel>
+                    <Input {...register("cpf")} placeholder="000.000.000-00" />
+                    {errors.cpf && (<FormErrorMessage variant="default" >{errors.cpf.message}</FormErrorMessage>)}
+                    <FormHelperText>{errors.cpf?.message}</FormHelperText>
                   </Box>
-                </Flex>
-                <Text fontFamily={"Inter"} fontWeight="medium" mt="5">Tipo de conta</Text>
-                <Flex justifyContent="space-around" alignItems="center" mt={4}>
-                  <Button
-                    bg={btnActive === "comprador" ? theme.colors.brand.brand1: theme.colors.greyScale.whiteFixed}
-                    color={btnActive === "comprador" ? theme.colors.greyScale.whiteFixed : theme.colors.greyScale.blackFixed}
-                    border={btnActive === "comprador" ? "none" : "solid"}
-                    borderWidth={btnActive === "comprador" ? "none" : "1px"}
-                    onClick={() => handleButtonClick("comprador")}>
-                      Comprador
-                   </Button>
-                  <Button
-                    bg={btnActive === "anunciante" ? theme.colors.brand.brand1 : theme.colors.greyScale.whiteFixed}
-                    color={btnActive === "anunciante" ? theme.colors.greyScale.whiteFixed : theme.colors.greyScale.blackFixed}
-                    border={btnActive === "anunciante" ? "none" : "solid"}
-                    borderWidth={btnActive === "anunciante" ? "none" : "1px"}
-                    onClick={() => handleButtonClick("anunciante")}>
-                      Anunciante
-                    </Button>
-                </Flex>
-                <Box mt={4}>
-                  <FormLabel  fontFamily={"Inter"} fontWeight="medium">Senha</FormLabel>
-                  <Input {...register("password")} placeholder="Digitar senha" />
-                </Box>
-                <Box mt={4}>
-                  <FormLabel fontFamily={"Inter"} fontWeight="medium">Confirmar Senha</FormLabel>
-                  <Input {...register("confirmationPassword")} placeholder="Digitar senha" />
-                </Box>
-                <Button mt="5" onClick={handleSubmit(onFormSubmit)} bg={theme.colors.brand.brand1} color={theme.colors.greyScale.whiteFixed} w="100%">
-                  Finalizar cadastro
-                </Button>
-              </FormControl>
-       </Flex>
+                  <Box mt={4}>
+                    <FormLabel fontFamily={"Inter"} fontWeight="medium">Celular</FormLabel>
+                    <Input {...register("cellphone")} placeholder="(DDD) 90000-0000"/>
+                  </Box>
+                  <Box mt={4}>
+                    <FormLabel fontFamily={"Inter"} fontWeight="medium">Data de nascimento</FormLabel>
+                    <Input {...register("birthDate")} placeholder="00/00/00" />
+                    {errors.birthDate && (<FormErrorMessage variant="default" >{errors.birthDate.message}</FormErrorMessage>)}
+                    <FormHelperText>{errors.birthDate?.message}</FormHelperText>
+                  </Box>
+                  <Box mt={4}>
+                    <FormLabel fontFamily={"Inter"} fontWeight="medium">Descrição</FormLabel>
+                    <Textarea {...register("description")} placeholder="Digitar descrição"/>
+                    {errors.description && (<FormErrorMessage variant="default" >{errors.description.message}</FormErrorMessage>)}
+                    <FormHelperText>{errors.description?.message}</FormHelperText>
+                  </Box>
+                  <Text fontFamily={"Inter"} fontWeight="medium" mt="5">Infomações de endereço</Text>
+                  <Box mt={4}>
+                    <FormLabel fontFamily={"Inter"} fontWeight="medium">CEP</FormLabel>
+                    <Input {...register("address.cep")} placeholder="00000.000" />
+                  </Box>
+                  <Flex justifyContent="space-between">
+                    <Box mt={4} mr={4}>
+                      <FormLabel fontFamily={"Inter"} fontWeight="medium">Estado</FormLabel>
+                      <Input {...register("address.state")} placeholder="Digitar Estado"/>
+                    </Box>
+                    <Box mt={4}>
+                      <FormLabel fontFamily={"Inter"} fontWeight="medium">Cidade</FormLabel>
+                      <Input {...register("address.city")} placeholder="Digitar cidade"/>
+                    </Box>
+                  </Flex>
+                  <Box mt={4}>
+                    <FormLabel fontFamily={"Inter"} fontWeight="medium">Rua</FormLabel>
+                    <Input {...register("address.street")} placeholder="Digitar Rua"/>
+                  </Box>
+                  <Flex justifyContent="space-between">
+                    <Box mt={4} mr={4}>
+                      <FormLabel fontFamily={"Inter"} fontWeight="medium">Número</FormLabel>
+                      <Input {...register("address.number")} placeholder="Digitar número"/>
+                    </Box>
+                    <Box mt={4}>
+                      <FormLabel fontFamily={"Inter"} fontWeight="medium">Complemento</FormLabel>
+                      <Input {...register("address.complement")} placeholder="Ex: apart 307"/>
+                    </Box>
+                  </Flex>
+                  <Text fontFamily={"Inter"} fontWeight="medium" mt="5">Tipo de conta</Text>
+                  <Flex justifyContent="space-around" alignItems="center" mt={4}>
+                    <Button
+                      bg={btnActive === "comprador" ? theme.colors.brand.brand1: theme.colors.greyScale.whiteFixed}
+                      color={btnActive === "comprador" ? theme.colors.greyScale.whiteFixed : theme.colors.greyScale.blackFixed}
+                      border={btnActive === "comprador" ? "none" : "solid"}
+                      borderWidth={btnActive === "comprador" ? "none" : "1px"}
+                      onClick={() => handleButtonClick("comprador")}>
+                        Comprador
+                     </Button>
+                    <Button
+                      bg={btnActive === "anunciante" ? theme.colors.brand.brand1 : theme.colors.greyScale.whiteFixed}
+                      color={btnActive === "anunciante" ? theme.colors.greyScale.whiteFixed : theme.colors.greyScale.blackFixed}
+                      border={btnActive === "anunciante" ? "none" : "solid"}
+                      borderWidth={btnActive === "anunciante" ? "none" : "1px"}
+                      onClick={() => handleButtonClick("anunciante")}>
+                        Anunciante
+                      </Button>
+                  </Flex>
+                  <Box mt={4}>
+                    <FormLabel  fontFamily={"Inter"} fontWeight="medium">Senha</FormLabel>
+                    <Input {...register("password")} placeholder="Digitar senha" />
+                  </Box>
+                  <Box mt={4}>
+                    <FormLabel fontFamily={"Inter"} fontWeight="medium">Confirmar Senha</FormLabel>
+                    <Input {...register("confirmationPassword")} placeholder="Digitar senha" />
+                  </Box>
+                  <Button mt="5" onClick={handleSubmit(onFormSubmit)} bg={theme.colors.brand.brand1} color={theme.colors.greyScale.whiteFixed} w="100%">
+                    Finalizar cadastro
+                  </Button>
+                </FormControl>
+            </Flex>
+       </Box>
        <Footer/>
     </>
   );
