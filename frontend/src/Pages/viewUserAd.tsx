ce6b6833c46bc47ccb2b84860d872a71b7606fb6
daniel delos silva <apeiron52@gmail.com>
2023-04-28 14:17:47 -0300

    refactor: page no logged

--- frontend/src/Pages/viewUserAd.tsx
+++ frontend/src/Pages/viewUserAd.tsx
@@ -1,10 +1,16 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Container, Text } from "@chakra-ui/react";
 import { Navbar } from "../components/Navbar";
 import AdPforfile from "../components/adProfile";
 import Footer from "../components/Footer";
 import { theme } from "../style/theme";
+import { useContext } from "react";
+import { CarAdsContext } from "../context/carAds.context";
+import { ProductCard } from "../components/ProductCard";
+import { iCarAdsInterface } from "../interfaces/carAds.interface";
 
 const ViewUserAd = () => {
+    const {carAds} = useContext(CarAdsContext)
+    console.log(carAds)
     return ( 
         <>
         <Navbar/>
@@ -12,6 +18,14 @@ const ViewUserAd = () => {
             <Box flex="1">
                 <AdPforfile/>
             </Box>
+            <Container maxW="80%">
+                <Text fontWeight={'bold'}>Anúncios</Text>
+                <Box display={'flex'} alignItems={'center'} justifyContent={"center"} p={5} gap={4}>
+                {carAds?.advertisements.map((ad: iCarAdsInterface) => (
+                <ProductCard key={ad.id}  id={ad.id} fipePrice={ad.fipePrice}  price={ad.price} isActive={ad.isActive} description={ad.description} brand={ad.brand} kilometers={ad.kilometers} images={ad.images} model={ad.model} year={ad.year} fuel_type={ad.fuel_type} user={ad.user} color={ad.color}/>
+                 ))}
+                </Box>
+            </Container>
             <Footer/>
         </Box>
         </>
--- frontend/src/components/adProfile/index.tsx
+++ frontend/src/components/adProfile/index.tsx
@@ -2,20 +2,24 @@ import { Box, Button, Link, Text } from "@chakra-ui/react";
 import { UserContext } from "../../context/userContext";
 import { useContext } from "react";
 import { theme } from "../../style/theme";
-// bg={theme.colors.greyScale.grey8}
+import { mockUsers } from "../../mock/mock.json";
 
 const AdPforfile = () => {
     const { user } = useContext(UserContext)
+    console.log(mockUsers[3])
 
     return (
-        <Box as='div' bgGradient="linear(to bottom, #4529E6 50%, #F1F3F5 50%)" h="350" display="flex" justifyContent="center" alignItems="center" position="relative" bottom="35.5%">
+        <Box as='div' bgGradient="linear(to bottom, #4529E6 50%, #F1F3F5 50%)" h="350" display="flex" justifyContent="center" alignItems="center" position="relative" bottom="35.5%" >
             <Box bg='#FDFDFD' w="75%" h="75%" p="9">
-                <Text as='span' bg="#4529E6" p='5' borderRadius='100%' color="white">{user?.name.split(" ").map((elem) => elem.charAt(0)).join("").slice(0, 2).toUpperCase()}</Text>
+                {/* <Text as='span' bg="#4529E6" p='5' borderRadius='100%' color="white">{user?.name.split(" ").map((elem) => elem.charAt(0)).join("").slice(0, 2).toUpperCase()}</Text> */}
+                <Text as='span' bg="#4529E6" p='5' borderRadius='100%' color="white">{mockUsers[3].name.split(" ").map((elem) => elem.charAt(0)).join("").slice(0, 2).toUpperCase()}</Text>
                 <Box as='div' display="flex">
-                    <Text mt="8" >{user?.name.toUpperCase()}</Text>
+                    {/* <Text mt="8" >{user?.name.toUpperCase()}</Text> */}
+                    <Text mt="8" >{mockUsers[3].name.toUpperCase()}</Text>
                     <Link ml="5" mt="9" bg="purple.50" fontSize="2xs" h="5" color="#4529E6" p="0.5" borderRadius="5">Anunciante</Link>
                 </Box>
-                <Text mt="5">{user?.description}</Text>
+                {/* <Text mt="5">{user?.description}</Text> */}
+                <Text mt="5">{mockUsers[3].description}</Text>
                 {/* <Button mt="5" bg="transparent" border="solid" borderColor="purple.600" borderWidth={2} color="purple.600" fontSize="xs">Criar anuncio</Button> */}
             </Box>
         </Box>);
